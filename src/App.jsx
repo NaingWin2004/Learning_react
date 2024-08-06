3b3477fdf1964aa86d5698941f6a201aa2bcf6ba
NaingWin <naingwin.dev@gamil.com>
2024-08-06 23:46:26 +0630

    useReducer smiple

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useReducer } from "react";
 
-
+const first = (state, action) => {
+    switch (action.type) {
+        case "PLUS":
+            return { ...state, count: state.count + 1 };
+        case "MINUS":
+          return {...state,count:state.count -1}
+    }
+};
 function App() {
-  
-  return (
-    <div className="text-3xl font-bold underline">Hello React</div>
-    )
-  
+    const [state, dispatch] = useReducer(first, { key: "", count: 0 });
+    return (
+        <>
+            <div className="flex my-5 justify-center items-center min-h-screen ">
+                <button
+                    className="btn"
+                    onClick={() => {
+                        dispatch({ type: "PLUS" });
+                    }}
+                >
+                    +
+                </button>
+                <span className="font-bold text-3xl mx-1.5">{state.count}</span>
+                <button className="btn" onClick={() => {
+                        dispatch({ type: "MINUS" });
+                    }}>-</button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
