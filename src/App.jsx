063661b92f487eccdbc7d929a3e5aa289b10c778
NaingWin <naingwin.dev@gamil.com>
2024-08-07 10:06:44 +0630

    showingData

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
-  
-  return (
-    <div className="text-3xl font-bold underline">Hello React</div>
-    )
-  
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const [todos, setTodos] = useState([]);
+    async function fetchData() {
+        let url = ` https://jsonplaceholder.typicode.com/todos`;
+        const res = await fetch(url);
+        const data = await res.json();
+        setTodos(data);
+    }
+    return (
+  <div className="overflow-x-auto">
+    <table className="table-fixed w-full max-w-3xl mx-auto bg-white shadow-md rounded-lg">
+        <thead className="bg-gray-200 border-b">
+            <tr>
+                <th className="border-r px-4 py-2">Id</th>
+                <th className="border-r px-4 py-2">Title</th>
+                <th className="border-r px-4 py-2">Completed</th>
+            </tr>
+        </thead>
+        <tbody className="text-center">
+            {todos.map(todo => (
+                <tr key={todo.id} className="border-b hover:bg-gray-100">
+                    <td className="border-r px-4 py-2">{todo.id}</td>
+                    <td className="border-r px-4 py-2">{todo.title}</td>
+                    <td className="border-r px-4 py-2">{todo.completed ? 'Yes' : 'No'}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+</div>
+
+    );
 }
 
-export default App
+export default App;
