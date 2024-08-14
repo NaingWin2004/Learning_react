ccaabc8886afd544c710a427e1f091178ce3d5fc
NaingWin <naingwin.dev@gamil.com>
2024-08-14 12:19:11 +0630

    toolkit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,27 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
-
+import { counterActions } from "../store/index.js";
 const Counter = () => {
     const dispath = useDispatch();
     const counter = useSelector(state => state.count);
     const isShow = useSelector(state => state.isShow);
 
     const increaseHandler = () => {
-        dispath({ type: "INCREASE" });
+        dispath(counterActions.increase());
     };
     const decreaseHandler = () => {
-        dispath({ type: "DECREASE" });
+        dispath(counterActions.decrease());
     };
     const increase5Handler = () => {
-        dispath({ type: "INCREASE5", amount: 5 });
+        dispath(counterActions.increase5(5));
     };
     const toggleHandler = () => {
-        dispath({ type: "TOGGLE" });
+        dispath(counterActions.toggle());
     };
     return (
         <div className="text-center mt-3 bg-sky-400 text-white font-black grid gap-3 p-2 mx-3 rounded">
             <h1>Redux Counter</h1>
-            {isShow &&  <p className="text-3xl">{counter}</p>}
-           
+            {isShow && <p className="text-3xl">{counter}</p>}
+
             <div className="flex justify-center gap-2">
                 <button
                     className="bg-white text-sky-300 px-2 py-1 rounded active:scale-90"
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,27 @@
-import { legacy_createStore as createStore } from "redux";
-const initialState = { count: 0, isShow: true };
-const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "INCREASE":
-            return {
-                count: state.count + 1,
-                isShow: state.isShow
-            };
-        case "DECREASE":
-            return {
-                count: state.count - 1,
-                isShow: state.isShow
-            };
-        case "INCREASE5":
-            return {
-                count: state.count + action.amount,
-                isShow: state.isShow
-            };
-        case "TOGGLE":
-            return {
-                count: state.count,
-                isShow: !state.isShow
-            };
-        default:
-            return state;
-    }
-};
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(counterReducer);
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: { count: 0, isShow: true },
+    reducers: {
+        increase(state) {
+            state.count++;
+        },
+        decrease(state) {
+            state.count--;
+        },
+        increase5(state,action) {
+          state.count = state.count + action.payload
+        },
+        toggle(state) {
+            state.isShow = !state.isShow;
+        }
+    }
+});
+
+export const counterActions = counterSlice.actions;
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
 export default store;
